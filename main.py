a1a3e19a825284e4088fad227fe6425a010f2b21
Emile Kamana <e.kamana@alustudent.com>
2021-09-23 10:47:42 +0200

    gathering user input

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+gender = input("Please enter your gender(M or F):")
+birthday_input = input('Enter your birthday in YYYY-MM-DD format')
+year, month, day = map(int, birthday_input.split('-'))
+birthday = datetime.date(year, month, day)
+print(birthday.weekday())
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
