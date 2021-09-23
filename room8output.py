c74a85f7f301ed95f0d87e46caaae32f604a11ce
fareedahokunade <90572113+fareedahokunade@users.noreply.github.com>
2021-09-23 11:05:45 +0200

    Update room8output.py

--- room8output.py
+++ room8output.py
@@ -1,36 +1,31 @@
 
-
-
-
-def ghana():
-
-    if gender == female:
-        if day == "Monday":
-            print("Your ghanaian alias is Adjoa ")
-        elif day == "Tuesday":
-            print("Your ghanaian alias is Abenaa ")
-        elif day == "Wednesday":
-            print("Your ghanaian alias is Akua")
-        elif day == "Thursday":
-            print("Your ghanaian alias is Yaa ")
-        elif day == "Friday":
-            print("Your ghanaian alias is Afia")
-        elif day == "Saturday":
-            print("Your ghanaian alias is Amma")
-        elif day == "Sunday":
-            print("Your ghanaian alias is Akosua ")
-    else:
-        if day == "Monday":
-            print("Your ghanaian alias is Kwasi ")
-        elif day == "Tuesday":
-            print("Your ghanaian alias is Kodjo ")
-        elif day == "Wednesday":
-            print("Your ghanaian alias is Kwabena")
-        elif day == "Thursday":
-            print("Your ghanaian alias is Kwaku ")
-        elif day == "Friday":
-            print("Your ghanaian alias is Yaw")
-        elif day == "Saturday":
-            print("Your ghanaian alias is Kofi")
-        elif day == "Sunday":
-            print("Your ghanaian alias is Kwame")
+if gender == female:
+    if day == "Monday":
+        print("Your ghanaian alias is Adjoa ")
+    elif day == "Tuesday":
+        print("Your ghanaian alias is Abenaa ")
+    elif day == "Wednesday":
+        print("Your ghanaian alias is Akua")
+    elif day == "Thursday":
+        print("Your ghanaian alias is Yaa ")
+    elif day == "Friday":
+        print("Your ghanaian alias is Afia")
+    elif day == "Saturday":
+        print("Your ghanaian alias is Amma")
+    elif day == "Sunday":
+        print("Your ghanaian alias is Akosua ")
+else:
+    if day == "Monday":
+        print("Your ghanaian alias is Kwasi ")
+    elif day == "Tuesday":
+        print("Your ghanaian alias is Kodjo ")
+    elif day == "Wednesday":
+        print("Your ghanaian alias is Kwabena")
+    elif day == "Thursday":
+        print("Your ghanaian alias is Kwaku ")
+    elif day == "Friday":
+        print("Your ghanaian alias is Yaw")
+    elif day == "Saturday":
+        print("Your ghanaian alias is Kofi")
+    elif day == "Sunday":
+        print("Your ghanaian alias is Kwame")
