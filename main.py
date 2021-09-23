fd29a950bc621104088964c84920a9249b6842b7
Emile Kamana <e.kamana@alustudent.com>
2021-09-23 10:50:37 +0200

    add comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import datetime
 
-gender = input("Please enter your gender(M or F):")
+gender = input("Please enter your gender(M or F):")    # getting input
 birthday_input = input('Enter your birthday in YYYY-MM-DD format')
-year, month, day = map(int, birthday_input.split('-'))
-birthday = datetime.date(year, month, day)
-print(birthday.weekday())
+year, month, day = map(int, birthday_input.split('-'))   # Splitting the input into separate year, month and date
+birthday = datetime.date(year, month, day)  # assigning the values to the birthday variable
+
 
